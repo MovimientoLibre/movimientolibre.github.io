cfc00fe77d0f09c7535b8e47f442e79fb005295b
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2016-02-24 13:17:27 -0600

    Nueva presentación El Poder de los Datos Abiertos.

---
+++ lib/Presentaciones/ElPoderDeLosDatosAbiertos.md
@@ -0,0 +1,11 @@
+
+<a href="el-poder-de-los-datos-abiertos/05-se-incrementaron-los-accidentes-viales.jpg"><img src="el-poder-de-los-datos-abiertos/05-se-incrementaron-los-accidentes-viales-small.jpg" alt="" class="diapositiva-sombra"></a> <a href="el-poder-de-los-datos-abiertos/08-los-ciudadanos-trabajan-con-los-datos-abiertos.jpg"><img src="el-poder-de-los-datos-abiertos/08-los-ciudadanos-trabajan-con-los-datos-abiertos-small.jpg" alt="" class="diapositiva-sombra"></a> <a href="el-poder-de-los-datos-abiertos/09-en-los-datos-abiertos-de-enero-a-julio-de-2015.jpg"><img src="el-poder-de-los-datos-abiertos/09-en-los-datos-abiertos-de-enero-a-julio-de-2015-small.jpg" alt="" class="diapositiva-sombra"></a>
+
+El 26 de febrero de 2016, [PIDES Innovación Social](http://www.pidesinnovacion.org/) y la [Embajada del Reino Unido en México](https://twitter.com/UKinMexico) organizan el [Encuentro de Ciudades Abiertas e Innovadoras 2016](http://www.pidesinnovacion.org/Iniciativas/ciudades-asi/).
+
+Con el honor de exponer las experiencias del municipio que represento, he publicado la presentación titulada "El Poder de los Datos Abiertos Gubernamentales" donde compartimos el camino recorrido para lograr que la información sobre **Seguridad Vial** aparezca en [datos.gob.mx](http://datos.gob.mx/).
+
+### Descargar
+
+* [Presentación PDF 5.6 MB](el-poder-de-los-datos-abiertos/poder-datos-abiertos-gubernamentales.pdf)
+* [Fuentes LaTeX tar.gz 5.9 MB](el-poder-de-los-datos-abiertos/poder-datos-abiertos-gubernamentales.tar.gz)
---
+++ lib/Presentaciones/ElPoderDeLosDatosAbiertos.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Movimiento Libre - ElPoderDeLosDatosAbiertos
+ *
+ * Copyright (C) 2015 Guillermo Valdés Lozano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package MovimientoLibre
+ */
+
+namespace Presentaciones;
+
+/**
+ * Clase ElPoderDeLosDatosAbiertos
+ */
+class ElPoderDeLosDatosAbiertos extends \Base\PublicacionSchemaBlogPosting {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        $this->nombre                     = 'El Poder de los Datos Abiertos';
+     // $this->autor                      = 'guivaloz';
+        $this->fecha                      = '2016-02-24T13:08';
+        // El nombre del archivo a crear
+        $this->archivo                    = 'el-poder-de-los-datos-abiertos';
+        // La descripción y claves dan información a los buscadores y redes sociales
+        $this->descripcion                = 'El Encuentro de Ciudades Abiertas e Innovadoras de PIDES Innovación Social es un evento que forma parte del programa de Ciudades ASI el cuál se centra en promover mecanismos para el desarrollo y la sustentabilidad a través de políticas públicas y proyectos de gobierno abierto a nivel local.';
+        $this->claves                     = 'Gobierno Abierto, Datos Abiertos, Seguridad Vial, Accidentes, Hackaton, Red Mexico Abierto';
+        // Ruta al archivo markdown con el contenido
+        $this->contenido_archivo_markdown = 'lib/Presentaciones/ElPoderDeLosDatosAbiertos.md';
+        // Para el Organizador
+        $this->categorias                 = array('Ciencia de Datos', 'Política');
+    } // constructor
+
+} // Clase ElPoderDeLosDatosAbiertos
+
+?>
