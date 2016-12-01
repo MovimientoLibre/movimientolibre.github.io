c84ac824f78d9cc2bed4bd9a73322a2fa1fd354d
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2016-12-01 08:33:47 -0600

    Agregué mi e-mail a las plantillas.

--- lib/Plantillas/MovimientoLibre ImprentaPublicacionesClasificadasPorCategorias.php
+++ lib/Plantillas/MovimientoLibre ImprentaPublicacionesClasificadasPorCategorias.php
@@ -2,7 +2,7 @@
 /**
  * Movimiento Libre - Imprenta Publicaciones Clasificadas por Categorías
  *
- * Copyright (C) 2016 Guillermo Valdés Lozano
+ * Copyright (C) 2016 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
